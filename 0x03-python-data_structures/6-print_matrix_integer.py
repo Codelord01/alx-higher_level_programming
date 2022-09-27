31eab75f3b5848962af58898a6297cbbb09d72f1
Codelord01 <davidawuniababio@gmail.com>
2022-09-27 20:22:16 +0000

    prints the integers in a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,13 @@ def print_matrix_integer(matrix=[[]]):
         sub_matrix_len = len(sub_matrix)
         index = 0
         while sub_matrix_len > 0:
-            print("{:d}".format(sub_matrix[index]), end=" ")
-            index += 1
-            sub_matrix_len -= 1
+            if index == sub_matrix_len + 1:
+                print("{:d}".format(sub_matrix[index]), end="")
+                index += 1
+                sub_matrix_len -= 1
+            else:
+                print("{:d} ".format(sub_matrix[index]), end="")
+                index += 1
+                sub_matrix_len -= 1
 
         print("")
