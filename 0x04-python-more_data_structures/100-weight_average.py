3418c45c7f9c4b1032fb1fb19e4c3e42e924701e
Codelord01 <davidawuniababio@gmail.com>
2022-09-29 05:52:19 +0000

    finds the average of pairs of values

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     total = 0
     denominator = 0
+    if len(my_list) == 0:
+        return 0
     for tup in my_list:
         total += (tup[0] * tup[1])
         denominator += tup[1]
