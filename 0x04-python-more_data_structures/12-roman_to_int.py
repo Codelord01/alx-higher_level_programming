e67745540a3601fb3da797e09b2845631a5bb59c
Codelord01 <davidawuniababio@gmail.com>
2022-09-29 05:41:08 +0000

    converts roman numerals to numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    string_len = len(roman_string)
-    loop_index = 0
-    roman_sum = roman_d[roman_string[loop_index]]
+    """
+    A function that converts a Roman numeral to an integer.
+    """
+    num_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
     if isinstance(roman_string, str):
-        if string_len == 1:
-            return roman_d[roman_string[loop_index]]
-        while string_len - loop_index != 1:
-            before = roman_d[roman_string[loop_index]]
-            ahead = roman_d[roman_string[loop_index + 1]]
-            if before < ahead:
-                roman_sum += - roman_d[roman_string[loop_index + 1]]
+        for i in range(len(roman_string) - 1):
+            if num_map[roman_string[i]] >= num_map[roman_string[i + 1]]:
+                total += num_map[roman_string[i]]
             else:
-                roman_sum += roman_d[roman_string[loop_index + 1]]
-            loop_index += 1
-        if roman_sum < 0:
-            return -roman_sum
-        else:
-            return roman_sum
+                total -= num_map[roman_string[i]]
+            i += 1
+        total += num_map[roman_string[i]]
+        return total
     return 0
