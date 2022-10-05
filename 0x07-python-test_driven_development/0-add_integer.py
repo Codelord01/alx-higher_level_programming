fcec2da105bc64a5e28b11815658735bb22a738c
Codelord01 <davidawuniababio@gmail.com>
2022-10-05 15:39:58 +0000

    adds two numbers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 
-"""Creates a function that adds two numbers"""
+"""Creates a function
+    The function takes two arguments
+    a and b and adds them up
+    And returns a result which is an int
+"""
 
 
 def add_integer(a, b=98):
-    """adds two numbers
+    """ argument 1 =a
+        argument 2 = b
     """
+
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
+
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
     return int(a) + int(b)
