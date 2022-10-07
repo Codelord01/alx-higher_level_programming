c1290d5bba01bd40d5deb35a2521e590671d0049
Codelord01 <davidawuniababio@gmail.com>
2022-10-07 12:09:48 +0000

    prints out all abject attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""creates a function that prints all object attributes"""
+
+
 def lookup(obj):
+    """function that prints out all object attritutes"""
     attributes = dir(obj)
     return attributes
