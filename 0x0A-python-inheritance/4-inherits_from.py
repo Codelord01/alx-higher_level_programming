9657d33bcc1f78d9cf17302784520ac307fad6f7
Codelord01 <davidawuniababio@gmail.com>
2022-10-07 12:57:23 +0000

    checks for derived or base class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""creates a function"""
+"""
+module only sub class of
+"""
 
 
 def inherits_from(obj, a_class):
-    """checks for base or derived class"""
+    """returns True if the object is an instance of a class
+       that inherited (directly or indirectly) from the specified class;
+       otherwise False.
+    """
     if type(obj) != a_class:
         return issubclass(type(obj), a_class)
     else:
-        False
+        return False
