92318a0744ac412ac7409f076925b4a4725d0c32
Codelord01 <davidawuniababio@gmail.com>
2022-10-18 16:11:20 +0000

    updates the update method to add keyword arguments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,20 +110,36 @@ class Rectangle(Base):
                                                        self.x, self.y,
                                                        self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
         takes in variable number of arguments
         """
         arg_len = len(args)
-        while arg_len > 0:
-            if arg_len == 1:
-                self.id = args[0]
-            elif arg_len == 2:
-                self.width = args[1]
-            elif arg_len == 3:
-                self.height = args[2]
-            elif arg_len == 4:
-                self.x = args[3]
-            elif arg_len == 5:
-                self.y = args[4]
-            arg_len -= 1
+        if arg_len != 0 and args:
+            while arg_len > 0:
+                if arg_len == 1:
+                    self.id = args[0]
+                elif arg_len == 2:
+                    self.width = args[1]
+                elif arg_len == 3:
+                    self.height = args[2]
+                elif arg_len == 4:
+                    self.x = args[3]
+                elif arg_len == 5:
+                    self.y = args[4]
+                arg_len -= 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
