1805b380e6ba3fb83a7d15615e9361a3400f4a6b
Codelord01 <davidawuniababio@gmail.com>
2022-10-18 13:52:19 +0000

    Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,9 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        init function for instance variables
+        """
         self.__width = width
         self.__height = height
         self.__x = x
@@ -20,32 +23,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        getter for width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        setter for width
+        """
         self.__width = value
 
     @property
     def height(self):
+        """
+        getter for height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        setter for height
+        """
         self.__height = value
 
     @property
     def x(self):
+        """
+        getter for x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        setter for x
+        """
         self.__x = value
 
     @property
     def y(self):
+        """
+        getter for y
+        """
         return self.__y
 
     @x.setter
     def y(self, value):
+        """
+        setter for y
+        """
         self.__y = value
