74c3fc6594c3bfb529fab0758e0da31a11dfb8ca
Codelord01 <davidawuni0@gmail.com>
2022-11-02 13:02:35 +0000

    creates a user and gives privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
 -- creates user user_0d_1 and grants all permissions
 CREATE USER 
-IF NOT EXISTS `user_0d_1`@`localhost` 
-IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`.`localhost` WITH GRANT OPTION;
+	IF NOT EXISTS `user_0d_1`@`localhost` 
+	IDENTIFIED BY `user_0d_1_pwd`;
+GRANT ALL PRIVILEGES 
+	ON *.* 
+	TO `user_0d_1`.`localhost` 
+	WITH GRANT OPTION;
