3808861b79356246bbd133e06e11f61e6345c4d8
Codelord01 <davidawuni0@gmail.com>
2022-11-02 15:14:07 +0000

    lists all cities in the database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 -- list all cities contained in database 'hbtn_0d_usa'
 SELECT `hbtn_0d_usa.cities.id`, `hbtn_0d_usa.cities.name`, `hbtn_0d_usa.states.name`
 FROM `hbtn_0d_usa.cities` NATURAL JOIN `hbtn_0d_usa.states`
+ORDER BY `hbtn_0d_usa.cities.id`;
