4f7d8403964a5f4ce4a2acbeb98128dcb1fcceee
Codelord01 <davidawuni0@gmail.com>
2022-11-02 13:33:21 +0000

    creates a database, adds a user, and grant SELECT privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- creates a database and adds a user with only SELECT privilege
 -- the user has only select privilege
 CREATE DATABASE 
-	IF NOT EXISTS hbtn_0d_2;
+	IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER 
 	IF NOT EXISTS `user_0d_2`@`localhost`
 	IDENTIFIED BY `user_0d_2_pwd`;
