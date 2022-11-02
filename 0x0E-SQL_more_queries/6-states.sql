056dd39a5d0074095d165c0a427f4f05a4f9ab5d
Codelord01 <davidawuni0@gmail.com>
2022-11-02 14:11:28 +0000

    creates a database and a table in it

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates a database called hbtn_0d_usa and a table called states in it
+CREATE DATABASE
+	IF NOT EXISTS `hbtn_0d_usa`;
+USE DATABASE `hbtn_0d_usa`;
+CREATE TABLE
+	IF NOT EXISTS `states`(`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, `name` VARCHAR(256))
